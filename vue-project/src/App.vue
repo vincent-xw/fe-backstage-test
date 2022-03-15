c5c011732fc5ebe72aedafc5f3c41276f1f8f77b
vincent-xw <jiaxuewen2006@163.com>
2022-03-15 20:04:00 +0800

    feat: 添加vue v-model的demo

---
+++ vue-project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <VModel v-model="value" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import VModel from "./components/VModel.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    VModel,
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  watch: {
+    value(oldValue, newValue) {
+      console.log("oldValue :>> ", oldValue);
+      console.log("newValue :>> ", newValue);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-project/src/components/VModel.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div.container
+    input(:value="value" @input="$emit('change', $event.target.value)")
+    p {{value}}
+    
+</template>
+<script>
+export default {
+  name: "testModel",
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  props: {
+    value: String,
+  },
+};
+</script>
